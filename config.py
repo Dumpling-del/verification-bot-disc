b20bcab8f60e3998ef0e2899189a130399b3748f
Dumpling-del <69228872+Dumpling-del@users.noreply.github.com>
2020-08-05 06:24:39 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+token = 'NzM4NDQ1NzE5MzgzNDQxNTE5.XyMBKw.PLEohLIScC2k6-Gk7ab4-lyjDKM'
+
+POST_ID = 739270845553311765
+
+EXCROLES = ()
+
+ROLES = { 
+    '🌌': 739270905057771532, #Роль верификации
+}
+
+MAX_ROLES_PER_USER = 2
